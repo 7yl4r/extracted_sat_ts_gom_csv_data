38427208b4b20a94758fb0726f52c3f02f9747a3
Dan Otis <dotis@seashell.marine.usf.edu>
2023-03-24 18:21:13 +0000

    + upload script

---
+++ upload_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# upload .csv files to github
+
+cd /srv/imars-objects/fgb/EXT_TS_MODA/OC && \
+  git add FGBdbv2_chlor_a_TS_MODA_daily_EFG.csv && \
+  git commit -m 'auto-upload csv files' && \
+  git push
