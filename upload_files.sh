9a33bd189f3b82cbe5b315d408e6611542ea0cf7
Tylar <email@tylar.info>
2023-06-02 14:02:28 -0400

    + git pull before commiting

--- upload_files.sh
+++ upload_files.sh
@@ -2,6 +2,7 @@
 # upload .csv files to github
 
 cd /srv/imars-objects/fgb/EXT_TS_MODA/OC && \
+  git pull && \
   git add FGBdbv2_chlor_a_TS_MODA_daily_EFG.csv && \
   git commit -m 'auto-upload csv files' && \
   git push
