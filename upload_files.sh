6de5da07b20921ebd7f448597c580ae47e1e1d98
Dan Otis <dotis@usf.edu>
2023-09-15 17:59:40 +0000

    add echo statements in hopes of debugging crontab fails

--- upload_files.sh
+++ upload_files.sh
@@ -7,8 +7,15 @@
 
 FILEDIR=/srv/imars-objects/tpa_pgs/rois/gom/extracted_sat_ts_gom_csv_data/
 
-cd $FILEDIR && \
-  git pull && \
-  git add ./data/*.csv && \
-  git commit -m 'auto-upload csv files' && \
-  git push
+set -e
+
+cd $FILEDIR
+
+echo pulling repo to avoid merge conflicts...
+git pull 
+echo adding csv files to local git...
+git add ./data/*.csv
+echo committing added files...
+git commit -m 'auto-upload csv files'
+echo pushing to git...
+git push
