afd9e9dadf09102652a2b23737067f2b60e5a945
Dan Otis <dotis@usf.edu>
2023-10-27 17:40:33 +0000

    add even more logging

--- upload_files.sh
+++ upload_files.sh
@@ -12,13 +12,15 @@ set -e
 # this setting helps avoid git asking for user input
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
+tee_cmd=tee -a upload_extractions.log
+
 cd $FILEDIR
-echo pulling repo to avoid merge conflicts...
-git pull 
-echo adding csv files to local git...
-git add ./data/*.csv
-echo committing added files...
-git commit -m 'auto-upload csv files'
-echo pushing to git...
-git push
+echo pulling repo to avoid merge conflicts... | tee upload_extractions.log
+git pull | $tee_cmd
+echo adding csv files to local git... | $tee_cmd
+git add ./data/*.csv | $tee_cmd
+echo committing added files... | $tee_cmd
+git commit -m 'auto-upload csv files' | $tee_cmd
+echo pushing to git... | $tee_cmd
+git push | $tee_cmd
 echo ============================================================
